9b590a3e949c809380f36799b4b2d5ed1e265f3a
Brendan Lynch <blynch@pentaho.com>
2015-06-22 21:12:53 -0400

    War App used to launch in Jetty
    Runs in <war name>/hello -- Context mapping in the web.xml does not
    remove the context mapping of the war namespace. This needs to be done
    in a seperate configuration. The war namespace is instead the root
    directory.

---
+++ helloworldWarApp/src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by brendan on 6/20/15.
+ */
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("Session = " + request.getSession(true).getId());
+    }
+}
